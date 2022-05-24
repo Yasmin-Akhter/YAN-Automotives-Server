5d6e0d243e526fc44b724facd8b481a45fa27ccd
Yasmin Akhter <yasminbd89@yahoo.com>
2022-05-24 13:03:38 +0600

    get added

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require('express/lib/response');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qn5o3z.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("manufacturer").collection("product");
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get('/products/:id', async (req, res) => {
+            id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product)
+        });
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('welcome');
 });
+
+
 app.listen(port, () => {
     console.log('port', port);
 })
